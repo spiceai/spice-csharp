468d581a69237986fdcd4ba596a1e14b9108e574
Kirill Khramkov <hramkir@gmail.com>
2024-06-13 07:29:57 +0400

    Fix local url

--- Spice/src/Config/SpiceDefaultConfigLocal.cs
+++ Spice/src/Config/SpiceDefaultConfigLocal.cs
@@ -2,5 +2,5 @@ namespace Spice.Config;
 
 internal static class SpiceDefaultConfigLocal
 {
-    public static readonly string FlightAddress = Environment.GetEnvironmentVariable("SPICE_LOCAL_FLIGHT_URL") ?? "grpc://localhost:50051";
+    public static readonly string FlightAddress = Environment.GetEnvironmentVariable("SPICE_LOCAL_FLIGHT_URL") ?? "http://localhost:50051";
 }
