22ac45a4afc0d6d99f1da65cdcedf64e5f031f0c
peasee <98815791+peasee@users.noreply.github.com>
2024-09-21 22:29:27 +1000

    feat: add x-spice-user-agent-header (#7)
    * feat: Add x-spice-user-agent
    
    * fix: Update test workflow to include more systems
    
    * fix: Remove 7.0 from test project
    
    * fix: User agent generation on Mac and Windows
    
    * fix: Windows OS type identifier

---
+++ Spice/src/Config/UserAgent.cs
@@ -0,0 +1,66 @@
+/*
+Copyright 2024 The Spice.ai OSS Authors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+namespace Spice.Config;
+
+internal static class UserAgent
+{
+    public static string agent()
+    {
+        // get OS type, release and machine type (x86, x64, arm, etc)
+        var os = Environment.OSVersion;
+        var osTypeStr = os.Platform.ToString();
+        var osVersion = os.VersionString;
+        osVersion = osVersion.Replace($"{osTypeStr} ", ""); // remove osType from version
+        if (osTypeStr == "Win32NT")
+        {
+            osVersion = osVersion.Replace("Microsoft Windows NT ", ""); // remove "Microsoft Windows NT " from version
+            osTypeStr = "Windows";
+        }
+
+        var osArch = System.Runtime.InteropServices.RuntimeInformation.OSArchitecture;
+
+        var osArchStr = "unknown";
+        // replace X64 with amd64, X86 with i386, etc
+        switch (osArch)
+        {
+            case System.Runtime.InteropServices.Architecture.X64:
+                osArchStr = "x86_64";
+                break;
+            case System.Runtime.InteropServices.Architecture.X86:
+                osArchStr = "i386";
+                break;
+            case System.Runtime.InteropServices.Architecture.Arm:
+                osArchStr = "arm";
+                break;
+            case System.Runtime.InteropServices.Architecture.Arm64:
+                osArchStr = "aarch64";
+                break;
+        }
+
+        // get the runtime version
+        var appVersion = typeof(SpiceClient).Assembly.GetName().Version;
+
+        // return the user agent string
+        return $"spice-dotnet {appVersion} ({osTypeStr}/{osVersion} {osArchStr})";
+    }
+}
